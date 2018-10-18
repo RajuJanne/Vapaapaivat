0a18ccc3640acf2a0392cf92fee5f052e420789f
RajuJanne <moby@insomnia.fi>
2018-10-18 13:06:13 +0300

    Alfa release

--- Solver.java
+++ Solver.java
@@ -1,4 +1,6 @@
-package JTR;
+package RajuJanne.vp;
+
+import java.util.Arrays;
 
 /**
  *
@@ -13,12 +15,22 @@ public class Solver {
     int _normalOff = 2; //arkena vapaalla
     int _specialOff = 5; //vkl vapaalla
     Calendar c;
-    
+
     public Solver ()
     {
-        
+
     }
-    
+
+    public Solver (int multip)
+    {
+        _weeks *= multip;
+        _workers *= multip;
+        _offdays *= multip;
+        _normalOff *= multip;
+        _specialOff *= multip;
+        // mitä vittua stintille tehää häh
+    }
+
     public Solver (int we, int wo, int od, int no, int so)
     {
         _weeks = we;
@@ -27,15 +39,39 @@ public class Solver {
         _normalOff = no;
         _specialOff = so;
     }
-    
+
     public void Initialize()
-    {        
-        c = new Calendar(_weeks, _workers, _offdays, _normalOff, _specialOff);
+    {
+        c = new Calendar(_weeks, _workers, _offdays, _normalOff, _specialOff, _maxStint);
         c.seedCalendar(_offdays);
+        /*
+        c.getStintBreakpoints();
+        c.getWorkerErrors();
+        c.getDayErrors();
+        c.calculateDayOffDifferentials();
+         */
     }
     public void Run()
     {
-        Initialize();
-        c.ToString();
+        // Ei tarkista vielä, että jokaisella työntekijällä on yksi vapaa viikonloppu, tai että työputket eivät ole liian pitkiä
+        Boolean stop = false;
+        do {
+            // annetaan Solve metodille 50 yritystä aikaa selvittää paskansa, tai alotetaan alusta.
+            Initialize();
+            for (int i = 0; i < 50; i++) {
+                c.runDayFunctions();
+                c.getStintBreakpoints();
+                //c.fixStints();
+                if(c.isSolved()) { stop = true;  break; }
+                else c.swapWorstDays();
+            }
+            if (stop) {
+                c.ToString();
+                System.out.println();
+                c.getStintBreakpoints();
+                System.out.println(c.get_breakpoints());
+                break;
+            }
+        }while (true);
     }
 }
